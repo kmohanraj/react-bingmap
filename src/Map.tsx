c0c5d292897e5fbaac4c99218dce8a9b4bb9a410
Mohanraj K <mohanraj.kandasamy@kohler.com>
2022-12-20 16:43:11 +0530

    Bing Map SDK

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,18 +1,64 @@
-import { FC, useCallback, useEffect } from "react";
+import React, { FC, useCallback, useEffect } from "react";
+import Icon from './images/legend-fm.svg'
+import pushPins from '../src/MockPushPins.json'
+
+
+type DescriptionType = {
+  name: string
+}
+
+type ContentType = {
+  title: string,
+  description: DescriptionType
+}
 
 const MapView: FC<any> = () => {
   
   let myWin = window as any
-  
   const initMap = useCallback(() => {
     var Maps = myWin.Microsoft.Maps
-    new Maps.Map('#bing-map', {
+    var map = new Maps.Map('#bing-map', {
       credentials: process.env.REACT_APP_BING_KEY,
-      center: new Maps.Location(51.50632, -0.12714),
-      zoom: 3
+      center: new Maps.Location(20.5937, 78.9629),
+      // bounds: Maps.LocationRect.fromEdges(49.234, 24.175, -65.573, -125.778),
+      zoom: 5
     })
+    let infobox = new Maps.Infobox(map.getCenter(), {
+      visible: false
+    })
+    infobox.setMap(map)
+    addPushPins(map, infobox, Maps)
   }, [])
 
+
+  const addPushPins = (map: any, infobox: any, Maps: any) => {
+    for (let ele in pushPins) {
+      let pin = new Maps.Pushpin(pushPins[ele].center, pushPinMarker(pushPins[ele].icon))
+      const data: ContentType = pushPins[ele]?.content
+      handleOnInfobox(pin, infobox, data, Maps)
+      map.entities.push(pin)
+    } 
+  }
+
+  const handleOnInfobox = (pin: any, infobox: any, data: ContentType, Maps: any) => {
+    Maps.Events.addHandler(pin, 'click', (e: any) => {
+      infobox.setOptions({
+        visible: true,
+        location: e.target.getLocation(),
+        title: data.title,
+        description: data.description.name
+      })
+    })
+  }
+
+  const pushPinMarker = (iconName: string) =>{
+    const mapIcons = ['F', 'IS', 'FM', 'KK', 'M'];
+    return {
+      icon: mapIcons.includes(iconName) && require(`./images/legend-${iconName?.toLocaleLowerCase()}.svg`)
+    }
+  }
+
+  // Initialize the Map
   myWin.initMap = initMap;
 
   useEffect(() => {
